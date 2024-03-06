6d44e3db3ffe1776638bcdbb0e6fc2eedad219bb
gkstnadl <gkstnadl12@naver.com>
2024-03-07 02:03:44 +0900

    Feat : TodoList 작성 완료

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,22 @@
+import { Todo } from '../types';
+
+interface TodoListProps {
+  todos: Todo[];
+  toggleTodo: (id: number) => void;
+}
+
+const TodoList: React.FC<TodoListProps> = ({ todos, toggleTodo }) => {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <div key={todo.id} onClick={() => toggleTodo(todo.id)}>
+          {todo.title}
+          {todo.contents}
+          <button>{todo.isCompleted ? '완료' : '취소'}</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
